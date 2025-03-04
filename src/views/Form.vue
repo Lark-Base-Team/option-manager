777d2c818036b3c1f9be86cca3f751e624c66ef2
Wang Chao <wangchao2423@gmail.com>
2025-03-04 08:18:18 +0800

    样式

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -3,7 +3,7 @@
  * @Author     : itchaox
  * @Date       : 2023-09-26 15:10
  * @LastAuthor : Wang Chao
- * @LastTime   : 2025-03-04 01:19
+ * @LastTime   : 2025-03-04 08:17
  * @desc       : 主要页面
 -->
 <script setup>
@@ -698,7 +698,7 @@
   .support-buttons {
     display: flex;
     gap: 12px;
-    margin-bottom: 20px;
+    margin-bottom: 10px;
   }
 
   .support-button {
@@ -714,7 +714,7 @@
   .label {
     display: flex;
     align-items: center;
-    margin-bottom: 20px;
+    margin-bottom: 10px;
 
     .text {
       width: 70px;
